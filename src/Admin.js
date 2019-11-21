9723cb8d8f21979043e8b37a1832c78a61fc1f66
jjsnc <zhuowangyi@163.com>
2019-11-21 10:53:52 +0800

    style

--- src/Admin.js
+++ src/Admin.js
@@ -10,16 +10,16 @@ import { Row, Col } from 'antd';
 import Header from './components/Header'
 import NavLeft from './components/NavLeft'
 import Footer from './components/Footer'
-/* import './style/common.less' */
+ import './style/common.less' 
 export default class Admin extends  Component{
    render(){
    	return (
         <div>
 		  <Row className="container">
-		      <Col span={3} className="nav-left" >
+		      <Col span={4} className="nav-left" >
                   <NavLeft />
 		      </Col>
-		      <Col span={21} className="main">
+		      <Col span={20} className="main">
                  <Header>
                  	这是头部
                  </Header>
