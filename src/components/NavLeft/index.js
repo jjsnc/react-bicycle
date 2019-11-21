1fc6b93f99272c028b4deac9c202a80bb844ab38
jjsnc <zhuowangyi@163.com>
2019-11-21 11:36:38 +0800

    menu 的实现

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -5,43 +5,51 @@
 * @Last Modified time: 2019-11-18 12:02:15
 */
 
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import { Menu, Icon } from 'antd';
 import MenuConfig from '../../config/menuConfig'
 import './index.less'
-console.log(MenuConfig,'MenuConfig')
 const { SubMenu } = Menu;
-function handleClick(e) {
-  console.log('click', e);
-}
 
-export default class NavLfet extends Component{
-	render(){
-		return (
-            <div>
-                 <div className="logo">
-                        {/* <img src="/assets/logo-ant.svg" alt=""/> */}
-                        <h1>Admin MS</h1>
-                  </div>
-            	  <Menu onClick={handleClick} theme="dark"  mode="vertical">
-				    <SubMenu
-				      key="sub1"
-				      title={
-				        <span>
-				          <Icon type="mail" />
-				          <span>Navigation One</span>
-				        </span>
-				      }
-				     >
-				      <Menu.Item key="1">Option 1</Menu.Item>
-				      <Menu.Item key="2">Option 2</Menu.Item>
-				    </SubMenu>
-				  </Menu>
-            </div>
-			)
-	}
+
+export default class NavLfet extends Component {
 	// 菜单渲染
-	renderMenu = ()=>{
-		
+	constructor(props) {
+		super(props)
+		this.state = {
+			'menuTreeNode': ""
+		}
 	}
+	render() {
+		return (
+			<div>
+				<div className="logo">
+					{/* <img src="/assets/logo-ant.svg" alt=""/> */}
+					<h1>Admin MS</h1>
+				</div>
+
+				<Menu theme="dark" mode="vertical">
+					{this.state.menuTreeNode}
+				</Menu>
+			</div>
+		)
+	}
+	renderMenu = (data) => {
+		console.log(data, 'data')
+		return data.map((item) => {
+			if (item.children) {
+				return <SubMenu key={item.key} title={item.title}>
+					{this.renderMenu(item.children)}
+				</SubMenu>
+			}
+			return <SubMenu key={item.key} title={item.title}></SubMenu>
+		})
+	}
+	componentDidMount() {
+		const menuTreeNode = this.renderMenu(MenuConfig)
+		this.setState({
+			menuTreeNode
+		})
+	}
+
 }
