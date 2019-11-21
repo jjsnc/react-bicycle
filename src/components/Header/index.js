e383d0937933d5c3b44fb307b4eea68de3136473
jjsnc <zhuowangyi@163.com>
2019-11-21 14:52:45 +0800

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@
 */
 
 import React, { Component } from 'react'
-// import { Row, Col } from 'antd';
+import { Row, Col } from 'antd';
 import Util from '../../utils/utils'
 import axios from '../../axios'
 import './index.less'
@@ -50,7 +50,27 @@ export default class Header extends Component {
 	}
 	render() {
 		return (
-			<div> {this.state.sysTime}
+			<div className='header'>
+				<Row className='header-top'>
+					<Col span={24}>
+						<span> 欢迎， {this.state.userName}</span>
+						<span> 退出</span>
+					</Col>
+				</Row>
+				<Row className='breadcrumb'>
+					<Col span={4} className="breadcrumb-title">
+						首页
+					</Col>
+					<Col span={20} className="weather">
+						<span className="date">{this.state.sysTime}</span>
+						<span className="weather-img">
+							<img src={this.state.dayPictureUrl} alt="" />
+						</span>
+						<span className="weather-detail">
+							{this.state.weather}
+						</span>
+					</Col>
+				</Row>
 			</div>
 		)
 	}
