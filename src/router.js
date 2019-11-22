14953eef960e2079d4cbc66197aec255b73a96de
jjsnc <zhuowangyi@163.com>
2019-11-22 14:50:34 +0800

    init

--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ import Home from './pages/home';
 import Buttons from './pages/ui/buttons'
 import Modals from './pages/ui/modals'
 // import NoMatch from './pages/nomatch'
-// import Loadings from './pages/ui/loadings'
+import Loadings from './pages/ui/loadings'
 // import Notice from './pages/ui/notice'
 // import Messages from './pages/ui/messages'
 // import Tabs from './pages/ui/tabs'
@@ -37,16 +37,35 @@ export default class ERouter extends Component {
                 <App>
                     <Switch>
                         <Route path='/login' component={Login}></Route>
-                        <Route path="/" render={()=>
+                        <Route path="/" render={() =>
                             <Admin>
                                 <Switch>
                                     <Route path='/home' component={Home} />
                                     <Route path="/ui/buttons" component={Buttons} />
                                     <Route path="/ui/modals" component={Modals} />
+                                    <Route path="/ui/loadings" component={Loadings} />
+                                    {/* <Route path="/ui/notification" component={Notice} />
+                                    <Route path="/ui/messages" component={Messages} />
+                                    <Route path="/ui/tabs" component={Tabs} />
+                                    <Route path="/ui/gallery" component={Gallery} />
+                                    <Route path="/ui/carousel" component={Carousel} />
+                                    <Route path="/form/login" component={FormLogin} />
+                                    <Route path="/form/reg" component={FormRegister} />
+                                    <Route path="/table/basic" component={BasicTable} />
+                                    <Route path="/table/high" component={HighTable} />
+                                    <Route path='/rich' component={Rich} />
+                                    <Route path="/city" component={City} />
+                                    <Route path="/order" component={Order} />
+                                    <Route path='/bikeMap' component={BikeMap} />
+                                    <Route path='/user' component={User} />
+                                    <Route path="/charts/bar" component={Bar} />
+                                    <Route path="/charts/pie" component={Pie} />
+                                    <Route path="/charts/line" component={Line} />
+                                    <Route path="/permission" component={Permission} /> */}
                                     {/* <Redirect to="/home" />
                                     <Route component={NoMatch} /> */}
                                 </Switch>
-                            </Admin>         
+                            </Admin>
                         } />
                     </Switch>
                 </App>
