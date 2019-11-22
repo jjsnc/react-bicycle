790ea92ac076b355683835d8654b0794dda73c0c
jjsnc <zhuowangyi@163.com>
2019-11-22 11:26:33 +0800

    init

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Admin from './Admin';
+// import Admin from './Admin';
+import Router from './router';
 // import Home from './route_demo/route1/Home';
 // import Router from './route_demo/route2/router';
 // import Router3 from './route_demo/route3/router';
 
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Admin />, document.getElementById('root'));
+ReactDOM.render(<Router />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/router.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
+import App from './App'
+import Login from './pages/login'
+import Admin from './Admin'
+// import Home from './pages/home';
+// import Buttons from './pages/ui/buttons'
+// import Modals from './pages/ui/modals'
+// import NoMatch from './pages/nomatch'
+// import Loadings from './pages/ui/loadings'
+// import Notice from './pages/ui/notice'
+// import Messages from './pages/ui/messages'
+// import Tabs from './pages/ui/tabs'
+// import Gallery from './pages/ui/gallery'
+// import Carousel from './pages/ui/carousel'
+// import FormLogin from './pages/form/login'
+// import FormRegister from './pages/form/register'
+// import BasicTable from './pages/table/basicTable'
+// import HighTable from './pages/table/highTable'
+// import Rich from './pages/rich'
+// import City from './pages/city/index'
+// import Order from './pages/order/index'
+// import Common from './common'
+// import OrderDetail from './pages/order/detail'
+// import BikeMap from './pages/map/bikeMap'
+// import User from './pages/user/index'
+// import Bar from './pages/echarts/bar/index'
+// import Pie from './pages/echarts/pie/index'
+// import Line from './pages/echarts/line/index'
+// import Permission from './pages/permission'
+// import renderEmpty from 'antd/lib/config-provider/renderEmpty'
+
+export default class ERouter extends Component {
+    render() {
+        return (
+            <HashRouter>
+                <App>
+                    <Switch>
+                        <Route path='/login' component={Login}></Route>
+                        <Route path='/admin' component={Admin}></Route>
+                    </Switch>
+                </App>
+            </HashRouter>
+        )
+    }
+}
