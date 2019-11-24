c9cad607c783dbddf164d13a0e088c02870c1e40
Damon <damon@jjsncMacBook-Pro.local>
2019-11-24 09:32:21 +0800

    reg

--- src/pages/form/register.js
+++ src/pages/form/register.js
@@ -1,4 +1,36 @@
-import React from {react}
+import React, { Component } from 'react'
 
-import {Card,Form,Button,Input,Checkbox,Radio,Select,Switch,DatePicker,TimePicker,Upload,Icon,message, InputNumber} from 'antd'
+import { Form, Input } from 'antd'
 
+
+class FormRegister extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { loading: true }
+    }
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <Form layout="horizontal">
+                <Form.Item>
+                    {
+                        getFieldDecorator('userName', {
+                            initialValue: '',
+                            rules: [
+                                {
+                                    required: true,
+                                    message: '用户名不能为空'
+                                }
+                            ]
+
+                        })(
+                            <Input placeholder='请输入用户名'/>
+                        )
+                    }
+                </Form.Item>
+            </Form>
+        )
+    }
+}
+
+export default Form.create()(FormRegister)
