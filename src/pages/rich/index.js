024436e0dc3ee13701e0fb2be243e7d33c0c7031
Damon <zhuowangyi@163.com>
2019-11-27 18:49:16 +0800

    rich

--- src/pages/rich/index.js
+++ src/pages/rich/index.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 export default class RichText extends Component {
-    render(){
+    constructor(props){
+        super(props)
+        this.state = {
+            editorState:''
+        }
+    }
+    onEditorStateChange(data){
+        console.log(data,'data')
+        this.setState({
+            editorState:data 
+        })
+    }
+    render() {
         return (
             <div>
-                123456
+                <Editor
+                    editorState={this.state.editorState}
+                    toolbarClassName="toolbarClassName"
+                    wrapperClassName="wrapperClassName"
+                    editorClassName="editorClassName"
+                    onEditorStateChange={this.onEditorStateChange}
+                />
             </div>
         )
     }
