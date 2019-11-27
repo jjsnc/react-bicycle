33c5191dbffbbc0fa51bfc6d4ef642255a04980b
Damon <zhuowangyi@163.com>
2019-11-27 18:59:46 +0800

    富文本编辑器 暂时不开发

--- src/pages/rich/index.js
+++ src/pages/rich/index.js
@@ -1,31 +1,11 @@
 import React, { Component } from 'react';
-import { Editor } from 'react-draft-wysiwyg';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
-export default class RichText extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            editorState:''
-        }
-    }
-    onEditorStateChange(data){
-        console.log(data,'data')
-        this.setState({
-            editorState:data 
-        })
-    }
-    render() {
-        return (
-            <div>
-                <Editor
-                    editorState={this.state.editorState}
-                    toolbarClassName="toolbarClassName"
-                    wrapperClassName="wrapperClassName"
-                    editorClassName="editorClassName"
-                    onEditorStateChange={this.onEditorStateChange}
-                />
-            </div>
-        )
-    }
+
+
+export default class richText extends Component {
+   render(){
+      return (
+         <div>此功能因为报错原因暂时不开发</div>
+      )
+   }
 }
