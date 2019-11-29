d097d5d5cbd53c05f3882993670ebf15190e1a0e
Damon <zhuowangyi@163.com>
2019-11-29 10:30:56 +0800

    redux

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import './index.css';
 // import App from './App';
 // import Admin from './Admin';
 import Router from './router';
+import { Provider } from 'react-redux'
+import configureStore from './redux/store/configureStore';
 // import Home from './route_demo/route1/Home';
 // import Router from './route_demo/route2/router';
 // import Router3 from './route_demo/route3/router';
 
 // import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<Router />, document.getElementById('root'));
+const store = configureStore();
+ReactDOM.render(
+    <Provider store={store}>
+        <Router />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
