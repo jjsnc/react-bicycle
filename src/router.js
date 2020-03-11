00f7dd4ebdd5717a08d681ff8bcd2f4fa70af484
Damon <zhuowangyi@163.com>
2020-03-11 16:06:51 +0800

    印度主要城市地图

--- src/router.js
+++ src/router.js
@@ -25,6 +25,7 @@ import OrderDetail from './pages/order/detail'
 import BikeMap from './pages/map/bikeMap'
 // import IndiaMap from './pages/map/IndiaMap'
 import WorldMap from './pages/map/worldMap'
+import BlueWorldMap from './pages/map/blueWorldMap'
 import User from './pages/user/index'
 import Bar from './pages/echarts/bar/index'
 import Pie from './pages/echarts/pie/index'
@@ -66,7 +67,9 @@ export default class ERouter extends Component {
                                     <Route path="/order" component={Order} />
                                     <Route path='/bikeMap' component={BikeMap} />
                                     {/* <Route path='/indiaMap' component={IndiaMap} />   */}
-                                    <Route path='/worldMap' component={WorldMap} />                                                                      
+                                    <Route path='/worldMap' component={WorldMap} />    
+                                    <Route path='/blueWorldMap' component={BlueWorldMap} />    
+                                                                                                      
                                     <Route path='/user' component={User} />
                                     <Route path="/charts/bar" component={Bar} />
                                     <Route path="/charts/pie" component={Pie} />
